06b198c348e372ffea5226ec42a46c28fd1624ff
Gokul <gokul.b@voxvalley.com>
2025-06-30 21:31:12 +0530

    All existing project file added.

--- app/src/main/java/com/example/javaLearningApp/ChatActivity/OpenAiApi.kt
+++ app/src/main/java/com/example/javaLearningApp/ChatActivity/OpenAiApi.kt
@@ -6,9 +6,9 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface OpenAiApi {
-    @Headers("Content-Type: application/json", "Authorization: Bearer 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")
+    @Headers("Content-Type: application/json",
+        "")
     @POST("chat/completions")
     fun getChatCompletion(@Body request: ChatRequest): Call<ChatResponse>
 }
 
-//git remote add origin https://github.com/Boradeg/OpenAiModule.git
